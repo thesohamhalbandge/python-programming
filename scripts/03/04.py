06d8b5c8dffa27d16d157781095a62f64b28bd9b
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-18 11:42:16 +0530

    Create 04.py

---
+++ scripts/03/04.py
@@ -0,0 +1,12 @@
+r = range(10)
+print(r, list(r))
+print(type(r))
+
+r1 = range(1, 11)
+print(list(r1))
+
+r2 = range(1, 11, 1) # start, stop, step 
+print(list(r2))
+
+r3 = range(1, 11, 2) 
+print(list(r3))
