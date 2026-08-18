fdb49d744adcc4847837bd58812eb45ea0346e57
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-18 09:44:13 +0530

    Create 03.py

---
+++ scripts/02/03.py
@@ -0,0 +1,32 @@
+# a = 10 
+# b = 20 
+# c = a + b
+# print(c)
+
+# --------------------------------- 
+
+# a = input("Enter first number:")
+# print(type(a))
+# x = int(a)
+# print(type(x))
+
+# b = input("Enter second number:")
+# print(type(b))
+# y = int(b)
+# print(type(y))
+
+# c = a + b
+# z = x + y 
+
+# print(c)
+# print(z)
+
+# --------------------------------------
+
+a1 = int(input("Enter first number: "))
+a2 = int(input("Enter second number: "))
+
+a3 = a1 + a2 
+
+print(a3)
+
