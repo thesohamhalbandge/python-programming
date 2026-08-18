5e63968961ef15c29840ee5558daaf320387a432
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-18 11:42:07 +0530

    Create 01.py

---
+++ scripts/03/01.py
@@ -0,0 +1,21 @@
+s = 'hyderabad'
+print(s)
+print(type(s))
+
+s1 = """hyderabad
+is one of the 
+best cities 
+in india"""
+print(s1)
+print(type(s1))
+
+# hyderabad's
+s2 = 'hyderabad\'s'
+print(s2)
+print(type(s2))
+
+s3 = "hyderabad's"
+print(s3)
+print(type(s3))
+
+# print(dir(str))
