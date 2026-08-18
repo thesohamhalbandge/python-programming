dff8d6633dd857da521607b75126b2d8ca7fbdef
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-18 09:44:19 +0530

    Create 04.py

---
+++ scripts/02/04.py
@@ -0,0 +1,17 @@
+a = 3 
+b = 5 
+c = complex(a, b) # (3+5j)
+print(c)
+print(type(c))
+
+print(c.real)
+print(c.imag)
+
+print(int(c.real))
+print(int(c.imag))
+
+a1 = 6 + 3j
+print(a1)
+print(type(a1))
+
+print(c + a1) # (9+8j)
