ad2ed20fa79f61b2a8b82c6d0c85cec405a1fecb
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-25 08:30:08 +0530

    Create 04.py

---
+++ scripts/05/04.py
@@ -0,0 +1,10 @@
+print(3 < 2 and 4 > 3) # False 
+print(3 > 2 and 4 > 3) # True
+
+print(5 > 3 or 4 < 3) # True 
+print(5 < 3 or 4 < 3) # False 
+
+print(not(4 < 5)) # False
+print(not(True)) # False 
+print(not(False)) # True 
+print(not(0)) # True 
