4a442d262e050add94ec90c3fb180f61551af273
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-25 08:30:15 +0530

    Create 07.py

---
+++ scripts/05/07.py
@@ -0,0 +1,16 @@
+a = 10
+print(bin(a)) # 0b1010
+
+b = 12
+print(bin(b)) # 0b1100
+
+print(a & b) # 8
+print(bin(a & b)) # 0b1000
+
+print(a | b) 
+print(a ^ b)
+
+a = 10
+print(a << 2)
+
+print(a >> 2)
