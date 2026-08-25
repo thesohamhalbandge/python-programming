23e53eebc83db5490809c89da4724333866dccb0
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-25 08:30:06 +0530

    Create 03.py

---
+++ scripts/05/03.py
@@ -0,0 +1,24 @@
+a = 5
+print(a)
+
+a += 2 # a = a + 2
+print(a) # 7
+
+a -= 2
+print(a) # 5
+
+a *= 2
+print(a) # 10 
+
+a /= 2
+print(a) # 5.0
+
+a %= 2
+print(a) # 1.0
+
+a = 7
+a **= 2 
+print(a) # 49
+
+a //= 2
+print(a) # 24
