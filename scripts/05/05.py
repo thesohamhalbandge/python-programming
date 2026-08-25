e572d3e1e43cdd528e08829904cd7db77ab8e9a1
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-25 08:30:10 +0530

    Create 05.py

---
+++ scripts/05/05.py
@@ -0,0 +1,8 @@
+l = [10, 20, 30, 40, 50]
+print(100 in l) # False
+print(10 in l) # True 
+print(10 not in l) # False 
+print(100 not in l) # True
+
+print("x" in "hyderabad")
+print("a" in "hyderabad")
