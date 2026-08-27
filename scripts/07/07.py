56dc243a5bf0f432b41342bf99f8290e9b3d1fc2
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-27 11:56:43 +0530

    Create 07.py

---
+++ scripts/07/07.py
@@ -0,0 +1,14 @@
+s = "python is very easy and it is oop language"
+print(s)
+print(s.capitalize()) # Python is very easy and it is oop language
+print(s.title()) # Python Is Very Easy And It Is Oop Language
+print(s.upper()) # PYTHON IS VERY EASY AND IT IS OOP LANGUAGE
+
+s = "HYDERABAD"
+print(s.lower()) # hyderabad
+
+print(s.isupper()) # True 
+print(s.islower()) # False 
+
+s = "PyThoN pRoGrAmMinG"
+print(s.swapcase()) # pYtHOn PrOgRaMmINg
