2528af11df4570cf9aa3d6eef64a1ed7e4a11733
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-27 11:56:38 +0530

    Create 05.py

---
+++ scripts/07/05.py
@@ -0,0 +1,10 @@
+s1 = "hello"
+s2 = "world"
+s3 = s1 + s2
+print(s3) # helloworld
+
+s3 = s1 + " " + s2 
+print(s3) # hello world 
+
+print(s1 * 3) # hellohellohello
+print((s1 + " ") * 3) # hello hello hello
