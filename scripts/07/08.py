70d04c444b0de3c75c64f712059ce00404274ba9
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-27 11:56:45 +0530

    Create 08.py

---
+++ scripts/07/08.py
@@ -0,0 +1,7 @@
+s = "python is very easy and it is oop language"
+print(s.count("is")) # 2 
+print(s.count(" ")) # 8
+print(s.count("a")) # 4
+print(s.count("x")) # 0
+
+print(s.replace(" ", "-")) # python-is-very-easy-and-it-is-oop-language
