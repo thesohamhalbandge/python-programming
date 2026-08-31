6acca5caa8dc6d74b83aaef12d1ada7476a0c189
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-08-31 14:44:34 +0530

    Create 11.py

---
+++ scripts/07/11.py
@@ -0,0 +1,12 @@
+s = "   python     "
+print(s)
+
+print(s.strip())
+print(s.lstrip())
+print(s.rstrip())
+
+s1 = "yyyyPythonyyyy"
+print(s1)
+print(s1.strip("y"))
+print(s1.lstrip("y"))
+print(s1.rstrip("y"))
