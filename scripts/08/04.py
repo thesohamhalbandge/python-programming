00a3b9165ab345d775e3c558acdecdee84490d30
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-09-01 13:44:40 +0530

    Create 04.py

---
+++ scripts/08/04.py
@@ -0,0 +1,6 @@
+l = [10, 20, 30, 10, 30, 10, 10, 30, 10, 30, 20, 10, 20, 10]
+print(l.count(10)) # 7
+print(l.count(100)) # 0
+
+print(l.index(30)) # 2 -> first occurence 
+# print(l.index(300)) -> ValueError
