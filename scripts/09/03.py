bf5b6fea6772232d622caa9604ae65c7d0664ac4
thesohamhalbandge <thesohamhalbandge@gmail.com>
2026-09-02 11:49:48 +0530

    Create 03.py

---
+++ scripts/09/03.py
@@ -0,0 +1,12 @@
+s = "hyderabad"
+print(s)
+print(type(s))
+
+print(tuple(s)) # ('h', 'y', 'd', 'e', 'r', 'a', 'b', 'a', 'd')
+
+l = [10, 20, 30, 40, 50]
+print(tuple(l)) # (10, 20, 30, 40, 50)
+
+t = ('a', 'b', 'c', 'd')
+print(str(t)) # ('a', 'b', 'c', 'd')
+print("".join(t)) # abcd
